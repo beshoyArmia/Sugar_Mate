53aea3aac18d9d31e42f1126c0ed8ab6a693f98c
Beshoy Armia <beshoarmia0@gmail.com>
2024-12-04 23:33:29 +0200

    handle firestore_sevie

--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_commerce/Features/auth/data/models/user_model.dart';
+import 'package:e_commerce/Features/auth/domain/entities/user_entity.dart';
 
 class firestoreService implements DatabaseService {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -7,9 +9,18 @@ class firestoreService implements DatabaseService {
       {required String path, required Map<String, dynamic> data}) async {
     await firestore.collection(path).add(data);
   }
+
+  @override
+  Future<Map<String, dynamic>> getData(
+      {required String path, required String id}) async {
+    var data = await firestore.collection(path).doc(id).get();
+    return data.data() as Map<String, dynamic>;
+  }
 }
 
 abstract class DatabaseService {
   Future<void> addData(
       {required String path, required Map<String, dynamic> data});
+  Future<Map<String, dynamic>> getData(
+      {required String path, required String id});
 }
